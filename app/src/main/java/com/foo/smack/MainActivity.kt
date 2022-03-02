109494e8886cd0bb97cdb38893436e4e2ab1bd2c
davidchhetri <david_chhetri@yahoo.com>
2022-03-02 11:48:01 -0500

    just before removing the fragments

--- app/src/main/java/com/foo/smack/MainActivity.kt
+++ app/src/main/java/com/foo/smack/MainActivity.kt
@@ -2,6 +2,7 @@ package com.foo.smack
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.View
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
@@ -39,11 +40,14 @@ class MainActivity : AppCompatActivity() {
         navView.setupWithNavController(navController)
     }
 
-/*    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }*/
+    fun loginBtnNavClicked(view: View){
+
+    }
+
+    fun addChannelClicked(view: View){
+
+
+    }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
