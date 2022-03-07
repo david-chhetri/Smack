fa13116a8c4e9ced983a319f84933f689edb3b92
davidchhetri <david_chhetri@yahoo.com>
2022-03-07 04:58:17 -0500

    Added create new user screen

--- app/src/main/java/com/foo/smack/LoginActivity.kt
+++ app/src/main/java/com/foo/smack/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.foo.smack
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -13,9 +14,12 @@ class LoginActivity : AppCompatActivity() {
 
     fun loginLoginBtnClicked(view: View){
 
+
     }
 
     fun loginCreateUserBtnClicked(view: View){
+        val createUserIntent = Intent(this, CreateUserActivity::class.java)
+        startActivity(createUserIntent)
 
     }
 }
