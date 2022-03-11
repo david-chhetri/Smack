963b1bd12978b3dc5e5f8f5d40c205b28b640270
davidchhetri <david_chhetri@yahoo.com>
2022-03-11 12:29:46 -0500

    login activity placing spinner and error Toast keyboard should dismiss

--- app/src/main/java/com/foo/smack/Controller/LoginActivity.kt
+++ app/src/main/java/com/foo/smack/Controller/LoginActivity.kt
@@ -1,49 +1,80 @@
 package com.foo.smack.Controller
 
+import android.content.Context
 import android.content.Intent
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.foo.smack.R
 import com.foo.smack.Services.AuthService
-import com.foo.smack.Services.UserDataService
 import kotlinx.android.synthetic.main.activity_login.*
-import kotlinx.android.synthetic.main.nav_header_main.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        loginSpinner.visibility = View.INVISIBLE
     }
 
-    fun loginLoginBtnClicked(view: View){
-        val email = loginEmailTxt.text.toString()
-        val password = loginPasswordTxt.text.toString()
-        //first login successfully
-        AuthService.loginUser(this,email,password){ loginSuccess ->
-            if(loginSuccess){
-                AuthService.findUserByEmail(this){ findingUserSuccess ->
-                    if(findingUserSuccess){
-                        finish()
-
+    fun loginLoginBtnClicked(view: View) {
+        enableSpinner(true)
+        hideKeyboard()
+        val email = loginEmailTxt.text.toString().trim()
+        val password = loginPasswordTxt.text.toString().trim()
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            AuthService.loginUser(this, email, password) { loginSuccess ->
+                if (loginSuccess) {
+                    AuthService.findUserByEmail(this) { findingUserSuccess ->
+                        if (findingUserSuccess) {
+                            enableSpinner(false)
+                            finish()
+                        } else {
+                            errorToast()
+                        }
                     }
-
+                } else {
+                    errorToast()
                 }
-
             }
-
+        } else {
+            Toast.makeText(this, "Please fill in both email and password.", Toast.LENGTH_LONG)
+                .show()
         }
-
-
-
     }
 
-    fun loginCreateUserBtnClicked(view: View){
+    fun loginCreateUserBtnClicked(view: View) {
         val createUserIntent = Intent(this, CreateUserActivity::class.java)
         startActivity(createUserIntent)
         finish()
 
+    }
+
+    fun errorToast() {
+        Toast.makeText(
+            this, "Something went wrong , please try again",
+            Toast.LENGTH_LONG
+        ).show()
+        enableSpinner(false)
+    }
+
+    fun enableSpinner(enable: Boolean) {
+        if (enable) {
+            loginSpinner.visibility = View.VISIBLE
+        } else {
+            loginSpinner.visibility = View.INVISIBLE
+        }
+        loginLoginBtn.isEnabled = !enable
+        loginCreateUserBtn.isEnabled = !enable
+
+    }
+
+    fun hideKeyboard(){
+        val inputManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        if(inputManager.isAcceptingText){
+            inputManager.hideSoftInputFromWindow(currentFocus.windowToken, 0)
+        }
     }
 }
